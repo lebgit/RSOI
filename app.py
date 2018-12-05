20d158894696707be14aa056efbd0c3da065e4eb
lebgit <32437110+lebgit@users.noreply.github.com>
2018-12-05 11:53:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def catch_all(path):
     return render_template('usage.html', error=True)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', port=port)
